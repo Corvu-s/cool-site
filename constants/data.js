2594e1d3d970f845ed1fdc25bc3c9b05ec6d2972
Corvu-s <roomtemp72@gmail.com>
2021-09-10 16:04:51 -0400

    site

---
+++ constants/data.js
@@ -0,0 +1,65 @@
+const userData = {
+  githubUsername: "Corvus",
+  name: "Luke Linigari",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "lukelinigari@gmail.com",
+  phone: "6474011850",
+  address: "Toronto, Ontario, Canada",
+  projects: [
+    {
+      title: "Stock Tracking App",
+      link: "https://github.com/Corvu-s/dividend-tracker",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "TTC Parking App",
+      link: "https://github.com/Corvu-s/EECS3311-Express-Parking-App",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Microcontroler Programming",
+      link: "https://github.com/Corvu-s/arduino-projects",
+      imgUrl: "/portfolio.png",
+    },    {
+      title: "Lego Creations ",
+      link: "https://github.com/Corvu-s/Lego-Digital-Designer-Models",
+      imgUrl: "/portfolio.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves new and interesting problems to solve!",
+    description: [
+      `I've been learning software engineering at YorkU for about 3 years now. Since then I've grown my skills and love learning new ones `,
+      `I really enjoy React, Node.js, MySQL,  Tailwind, C, just to name a few. I've focused mostly on react these days making cool things along the way. Software is always changins so I can't wait for the next cool new thing!`,
+    ],
+    currentProject: "Stock Tracking App",
+    currentProjectUrl: "https://github.com/Corvu-s/dividend-tracker",
+  },
+  experience: [
+    {
+      title: "Post Secondaty",
+      company: "Lassonde School of Engineering",
+      year: "2018-2023",
+      companyLink: "",
+      desc: "I started this program egar to get into software development and i'm really happy with my time here! Not graduated yet but it's soon approaching!",
+    },
+    {
+      title: "High School",
+      company: "Emily Carr Secondary, Vaughan ON",
+      year: "2018",
+      companyLink: "",
+      desc: "The computer courses were my fav. Can't leave physics and chemistry out, they rock too!",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1X5ADvQfoz_n-4RgTN8rX58vsPVWE7aI9/view?usp=sharing",
+  socialLinks: {
+    linkedin: "www.linkedin.com/in/luke-linigari",
+    github: "https://github.com/Corvu-s",
+ 
+  },
+};
+
+export default userData;
